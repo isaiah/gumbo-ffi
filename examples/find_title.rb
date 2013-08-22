44eb75a8f50766a6e6972dc3d9e57844e08e9442
isaiah <issaria@gmail.com>
2013-08-22 23:04:28 +0200

    make example more rubish

--- examples/find_title.rb
+++ examples/find_title.rb
@@ -1,27 +1,23 @@
 $:.unshift(File.expand_path("../../lib", __FILE__))
 require 'gumbo'
 
-text = IO.read(File.expand_path("../../../../c/gumbo-parser/docs/html/index.html", __FILE__))
+text = IO.read(File.expand_path("../test_data/index.html", __FILE__))
+
 doc = Gumbo.parse(text)
 
-head = nil
 root_children = doc.root.children
-root_children.each do |child|
-  if child.type == :element && child.content.tag == :HEAD
-    head = child
-    break
-  end
+head = root_children.find do |child|
+        child.type == :element && child.content.tag == :HEAD
 end
 
 head_children = head.content.children
-head_children.each do |child|
-  if child.type == :element && child.content.tag == :TITLE
-          if child.content.children.length != 1
-                  puts "<empty title>"
-                  break
-          end
-          title = child.content.children[0]
-          puts title.content
-          break
-  end
+title_node = head_children.find do |child|
+        child.type == :element && child.content.tag == :TITLE
 end
+
+if title_node.content.children.length != 1
+        puts "<empty title>"
+end
+
+title = title_node.content.children[0]
+puts title.content
